6589eb4a72ccc434a5fb18c9e6ec4bdfcdfc2c9b
Kazuo Hayakawa <gg.e.river@gmail.com>
2017-08-12 17:19:16 +0900

    Pythonの基礎知識（Basic knowledge of python）
    変数の型についてのサンプルです。
    Sample about variable type.

---
+++ abc/variable_type.py
@@ -0,0 +1,20 @@
+#
+# 変数の型について
+#
+
+doc = """
+Pythonの変数の型をいくつか確認してみます。
+""".strip()
+
+print(doc)
+
+"""
+https://docs.python.jp/3/library/stdtypes.html
+"""
+
+print("int")
+print("string")
+print("array")
+print("hash")
+
+
